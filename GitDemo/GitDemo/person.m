86f82a4b8919f9bb1952bec080b31206b39cff46
Qianfeng <qianfeng@haliluya.local>
2016-04-01 15:10:23 +0800

    第二次操作

---
+++ GitDemo/GitDemo/person.m
@@ -0,0 +1,21 @@
+//
+//  person.m
+//  GitDemo
+//
+//  Created by Qianfeng on 16/4/1.
+//  Copyright (c) 2016年 李天亚. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+int a = 11;
+@end
